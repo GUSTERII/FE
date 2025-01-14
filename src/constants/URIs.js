276dbcdbe1361cf7ee21f123287c7a449eafa2bd
VictorBalanWork <victor.balan@assist.ro>
2025-01-14 17:30:56 +0200

    Fix auth 2

--- src/constants/URIs.js
+++ src/constants/URIs.js
@@ -1,2 +1,3 @@
-// export const BE_URL = 'https://gusteriibe-c0brancqaufvfyc5.canadacentral-01.azurewebsites.net';
-export const BE_URL = "http://localhost:8081";
+export const BE_URL =
+  "https://gusteriibe-c0brancqaufvfyc5.canadacentral-01.azurewebsites.net";
+// export const BE_URL = "http://localhost:8081";
